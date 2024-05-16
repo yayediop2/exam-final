eac0aa2933ea66a46814db5f2e931fcfc15574b8
yayediop <mamyta2000@outlook.fr>
2024-05-16 20:01:52 +0000

    commit

--- capitalizing/src/lib.rs
+++ capitalizing/src/lib.rs
@@ -1,8 +1,28 @@
 pub fn capitalize_first(input: &str) -> String {
+    if input.is_empty() {return "".to_string();}
+    input[..1].to_ascii_uppercase() + &input[1..].to_ascii_lowercase()
 }
 
 pub fn title_case(input: &str) -> String {
+    let mut res = String::new();
+    for word in input.split_whitespace() {
+        res.push_str(&capitalize_first(word));
+        res.push(' ');
+    }
+    let res = res.trim();
+    res.to_string()
 }
 
 pub fn change_case(input: &str) -> String {
+    let mut res = String::new();
+    for c in input.chars() {
+        if c.is_ascii_lowercase() {
+            res.push(c.to_ascii_uppercase());
+        } else if  c.is_ascii_uppercase() {
+            res.push(c.to_ascii_lowercase());
+        } else {
+            res.push(c);
+        }
+    }
+    res
 } 
